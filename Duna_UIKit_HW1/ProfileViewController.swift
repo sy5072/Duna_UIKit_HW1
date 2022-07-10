7589a08101cbc844b2a9585eb0e9050c9c3467d2
Nick(Seungyun Kim) <sy5072@naver.com>
2022-07-10 23:11:17 +0900

    레이아웃 구성

--- Duna_UIKit_HW1/ProfileViewController.swift
+++ Duna_UIKit_HW1/ProfileViewController.swift
@@ -11,8 +11,6 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func goBack(_ sender: Any) {
