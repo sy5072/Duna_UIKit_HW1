d19007745bf4095d7236fe4312d3db75bef874c2
Nick(Seungyun Kim) <sy5072@naver.com>
2022-07-10 20:53:54 +0900

    modal view 구현

---
+++ Duna_UIKit_HW1/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  Duna_UIKit_HW1
+//
+//  Created by Seungyun Kim on 2022/07/10.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func goBack(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
